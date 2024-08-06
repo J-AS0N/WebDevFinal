90240f6e2d07a741cb340f15aa404aad95454ecc
Jason <jasonweng44@gmail.com>
2024-08-06 11:57:48 -0400

    Designed AllTasksView Page

--- src/Pages/AllTasksView.jsx
+++ src/Pages/AllTasksView.jsx
@@ -1,7 +1,59 @@
-function AllTasksView() {
-    return(
-        <h2> All tasks view</h2>
+import {useState} from 'react'
+import AllTasksViewCSS from '../styles/AllTasksView.module.css'
 
+function AllTasksView() {
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState("");
+
+
+    function handleInput(event) {
+        setNewTask(event.target.value);
+    }
+
+    function addTask() {
+        if (newTask.trim() === "") {
+            alert("Please fill out the field.")
+            return;
+        }
+        setTasks(t => [...t, newTask]);
+        setNewTask("");
+    }
+
+    function handleEnter(event) {
+        if(event.key === 'Enter') {
+            addTask();
+        }
+    }
+
+    function deleteTask(index) {
+        setTasks(t => t.filter((task, i) => i !== index));
+    }
+
+
+
+    return(
+        <div className={AllTasksViewCSS.container}>
+            <div className={AllTasksViewCSS.container2}>
+                <h1 className={AllTasksViewCSS.text}> Tasks </h1>    
+                <input className={AllTasksViewCSS.input} type="text" placeholder="Enter Task" value={newTask} onChange={handleInput} onKeyDown={handleEnter}></input>
+                <button className={AllTasksViewCSS.add} onClick={addTask}> Add </button>
+            </div>
+
+            <div className={AllTasksViewCSS.container3}>
+                <ol> 
+                    {tasks.map((task, index) => 
+                        <li key={index}>
+                            {task}
+                            <div className={AllTasksViewCSS.buttoncontainer}>
+                                <button className={AllTasksViewCSS.priority}> Priority Level: <br/>High </button>
+                                <button className={AllTasksViewCSS.status}> Status: <br/> COMPLETE </button>
+                                <button className={AllTasksViewCSS.delete} onClick={() => deleteTask(index)}> Delete </button>
+                            </div>
+                        </li>
+                    )}
+                </ol>
+            </div>
+        </div>
     );
 }
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,7 @@ import HomeCSS from '../styles/Home.module.css'
 function Home() {
     return(
     <div className={HomeCSS.container}>
-        <h1> WELCOME</h1>
+        <h1> WELCOME </h1>
         <Link to="/AllEmployeesView" style={{textDecoration: 'none'}}> <button className={HomeCSS.employees}> Employees </button> </Link>
         <Link to="/AllTasksView" style={{textDecoration: 'none'}}> <button className={HomeCSS.tasks}> Tasks </button> </Link>
     </div>
