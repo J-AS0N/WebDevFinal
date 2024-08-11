11d5d1c2da326902dd069ee1a1237972d849b31f
Jason <jasonweng44@gmail.com>
2024-08-11 11:34:14 -0400

    Fetching from API works + gave colors based on completion/priority level

--- src/components/AllEmployeesContainer.jsx
+++ src/components/AllEmployeesContainer.jsx
@@ -4,16 +4,22 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchEmployees, deleteEmployee } from '../Redux Store/EmployeeSlice';
 
 function AllEmployeesContainer() {
+  console.log('AllEmployeesContainer rendered');
   const employees = useSelector((state) => state.employees);
   const dispatch = useDispatch();
 
   useEffect(() => {
+    console.log("Dispatching fetchEmployees")
     dispatch(fetchEmployees());
   }, [dispatch]);
 
   const handleDelete = (id) => {
     dispatch(deleteEmployee(id));
   };
+
+  if (!employees) {
+    return <div> Loading... </div>;
+  }
 
   return (
     <AllEmployeesView employees={employees} onDelete={handleDelete} />
